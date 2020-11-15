875c3c0abdfd4f30476ea8096cdce8a4d13580dc
september <zzzhen1994@gmail.com>
2020-11-15 17:38:24 +0800

    refactor: add config package

--- cmd/richman/main.go
+++ cmd/richman/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"log"
-	"os"
-	"os/user"
-	"path/filepath"
 	"strings"
 	"time"
 
-	"gopkg.in/yaml.v2"
-
 	"github.com/zhenzou/richman"
+	"github.com/zhenzou/richman/conf"
 	"github.com/zhenzou/richman/utils"
 )
 
@@ -24,7 +19,6 @@ var (
 )
 
 func init() {
-
 	if strings.EqualFold(debug, "true") {
 		log.SetFlags(log.LstdFlags | log.Lshortfile)
 	} else {
@@ -32,82 +26,7 @@ func init() {
 	}
 }
 
-const confFile = "/.richman/conf.yaml"
-
-const confSample = `
-tasks:
-  get-stocks:
-    type: "stocks"
-    params:
-      provider: "sina"
-      stocks: [ "sz002594" ]
-
-jobs:
-  monitor-stocks:
-    schedule:
-      type: "cron"
-      params:
-        cron: '*/6 * * * * *'
-    task: "get-stocks"
-`
-
-func buildConfPath() string {
-	u, err := user.Current()
-	if err != nil {
-		log.Println("get current user error:", err.Error())
-		utils.Die()
-	}
-	return u.HomeDir + confFile
-}
-
-func loadConf() Config {
-	path := buildConfPath()
-	err := os.MkdirAll(filepath.Dir(path), os.ModePerm)
-	if err != nil {
-		log.Println("write conf file error:", err.Error())
-		utils.Die()
-	}
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		err := ioutil.WriteFile(path, []byte(confSample), 0644)
-		if err != nil {
-			log.Println("write conf file error:", err.Error())
-			utils.Die()
-		}
-	}
-	file, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Println("read conf file error:", err.Error())
-		utils.Die()
-	}
-	conf := Config{}
-	err = yaml.Unmarshal(file, &conf)
-	if err != nil {
-		log.Println("read conf file error:", err.Error())
-		utils.Die()
-	}
-	return conf
-}
-
-type Config struct {
-	Tasks map[string]Task
-	Jobs  map[string]Job
-}
-
-type Task struct {
-	Type   string         `yaml:"type"`
-	Params richman.Params `yaml:"params"`
-}
-
-type Job struct {
-	Schedule struct {
-		Type   string         `yaml:"type"`
-		Params richman.Params `yaml:"params"`
-	} `yaml:"schedule"`
-	Task string `yaml:"task"`
-}
-
-func initTasks(conf Config) map[string]richman.Task {
+func initTasks(conf conf.Config) map[string]richman.Task {
 	tasks := make(map[string]richman.Task)
 
 	for name, task := range conf.Tasks {
@@ -129,7 +48,7 @@ func initTasks(conf Config) map[string]richman.Task {
 	return tasks
 }
 
-func initJobs(conf Config, tasks map[string]richman.Task) map[string]richman.Job {
+func initJobs(conf conf.Config, tasks map[string]richman.Task) map[string]richman.Job {
 
 	jobs := map[string]richman.Job{}
 
@@ -159,11 +78,11 @@ func main() {
 
 	log.Printf("release: %s, repo: %s, commit: %s\n", release, repo, commit)
 
-	conf := loadConf()
+	config := conf.Load()
 
-	tasks := initTasks(conf)
+	tasks := initTasks(config)
 
-	jobs := initJobs(conf, tasks)
+	jobs := initJobs(config, tasks)
 
 	monitor := richman.NewMonitor()
 
---
+++ conf/config.go
@@ -0,0 +1,101 @@
+package conf
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/zhenzou/richman/utils"
+)
+
+const confFile = "/.richman/conf.yaml"
+
+const confSample = `
+tasks:
+  get-stocks:
+    type: "stocks"
+    params:
+      provider: "sina"
+      stocks: [ "sz002594" ]
+
+jobs:
+  monitor-stocks:
+    schedule:
+      type: "cron"
+      params:
+        cron: '*/6 * * * * *'
+    task: "get-stocks"
+`
+
+type Config struct {
+	Tasks map[string]Task
+	Jobs  map[string]Job
+}
+
+type Task struct {
+	Type   string `yaml:"type"`
+	Params Params `yaml:"params"`
+}
+
+type Job struct {
+	Schedule struct {
+		Type   string `yaml:"type"`
+		Params Params `yaml:"params"`
+	} `yaml:"schedule"`
+	Task string `yaml:"task"`
+}
+
+func Load() Config {
+	path := buildConfPath()
+	err := os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	if err != nil {
+		log.Println("write conf file error:", err.Error())
+		utils.Die()
+	}
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		err := ioutil.WriteFile(path, []byte(confSample), 0644)
+		if err != nil {
+			log.Println("write conf file error:", err.Error())
+			utils.Die()
+		}
+	}
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Println("read conf file error:", err.Error())
+		utils.Die()
+	}
+	conf := Config{}
+	err = yaml.Unmarshal(file, &conf)
+	if err != nil {
+		log.Println("read conf file error:", err.Error())
+		utils.Die()
+	}
+	return conf
+}
+
+func buildConfPath() string {
+	u, err := user.Current()
+	if err != nil {
+		log.Println("get current user error:", err.Error())
+		utils.Die()
+	}
+	return u.HomeDir + confFile
+}
+
+type Params struct {
+	unmarshal func(interface{}) error
+}
+
+func (msg *Params) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	msg.unmarshal = unmarshal
+	return nil
+}
+
+func (msg *Params) Unmarshal(v interface{}) error {
+	return msg.unmarshal(v)
+}
